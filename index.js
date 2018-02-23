00a4f4b2c8533507f91b1890275a1bedbce40129
Bryan Bell <bbell1333@gmail.com>
2018-02-23 03:39:17 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -90,17 +90,17 @@ function createRock(x) {
     }else{
       rock.remove();
     }
-    window.requestAnimationFram(moveRock);
+    
     /**
      * But if the rock *has* reached the bottom of the GAME,
      * we should remove the rock from the DOM
      */
-     return rock;
+     
   }
 
   // We should kick of the animation of the rock around here
 
- 
+ window.requestAnimationFram(moveRock);
 
   // Add the rock to ROCKS so that we can remove all rocks
   // when there's a collision
