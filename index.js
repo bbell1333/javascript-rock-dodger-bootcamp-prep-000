8051e6547c7b0ad22853f4aa949bd206e7ae97b6
Bryan Bell <bbell1333@gmail.com>
2018-02-23 03:36:29 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -77,6 +77,7 @@ function createRock(x) {
      * If a rock collides with the DODGER,
      * we should call endGame()
      */
+    rock.style.top=`${top}+
     if (checkCollision(rock)){
       return endGame();
     }
