c85369e7afbe13b779ed325720614583a47a1cac
Bryan Bell <bbell1333@gmail.com>
2018-02-23 03:53:04 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -93,7 +93,7 @@ function endGame() {
 
   ROCKS.forEach(function(rock) { rock.remove() })
 
-  document.removeEventListener('keydown', moveDodger)
+  document.removeEventListener('keydown', moveDodger())
 
   
 
